6bd28c98576452681ebd0de358821121289b61c7
johnkayconsultant <johnkayconsultant@gmail.com>
2026-07-15 15:51:47 +0100

    update about

--- src/assets/Components/Pages/About.tsx
+++ src/assets/Components/Pages/About.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import button from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
-function About() {
-  return (
-    <div>About</div>
-  )
+
+const Stat = [
+        {value:"8+", label:"Years of Experience"},
+        {value:"98+", label:"Satisfaction"},
+        {value:"500+", label:"Project Completed"},
+        {value:"150+", label:"Expert Teams"},
+        ];
+
+const  value = [
+  
+
+];
+
+
+
+const About =()=>{
+
 }
 
-export default About
+export default About;
